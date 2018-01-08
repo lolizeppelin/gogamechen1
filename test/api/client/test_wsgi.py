fd2fde9b050c9986788df9cd5dd1c599cd38bf33
lolizeppelin <lolizeppelin@gmail.com>
2018-01-08 11:08:33 +0800

    参数默认值修改

--- test/api/client/test_wsgi.py
+++ test/api/client/test_wsgi.py
@@ -25,6 +25,29 @@ httpclient = ManagerClient(wsgi_url, wsgi_port, timeout=30)
 client = GogameChen1DBClient(httpclient)
 
 
+def file_create():
+    cross = {'address': 'http://172.17.0.3/crosserver.zip',
+                                      'size': 3640656,
+                                      'md5': '37a92e3670793d9fe7d7b4166405b7c0',
+                                      'crc32': '2328264931'}
+
+    gm = {'address': 'http://172.17.0.3/gmserver.zip',
+          'size': 3810087, 'md5': 'ccfccde94771f8a383055ae0ca54f395', 'crc32': '3345614830'}
+
+    game = {'address': 'http://172.17.0.3/gameserver.zip',
+            'size': 21363437, 'md5': '4aef586427aa0fab051d19c930a87c0b', 'crc32': '3527425352'}
+
+    print client.objfile_create(objtype=common.GAMESERVER, subtype=common.APPFILE,
+                                version='20180104.001',
+                                body=game)
+
+def file_index():
+    print client.objfiles_index()
+
+def send_file(uuid):
+    print client.send_file_to_agents(agent_id=6, file_id=uuid,
+                                     body={'request_time': int(time.time())})
+
 def group_index_test():
     print client.groups_index()
 
@@ -47,7 +70,9 @@ def game_show(entity):
 
 
 def crosss_create():
-    print client.crosss_create(group_id=1)
+    print client.crosss_create(group_id=1, body={'impl': 'local',
+                                                 'objfile': {'subtype': 'appfile',
+                                                             'version': '20180104.001'}})
 
 def cross_delete(entity):
     print client.cross_delete(group_id=1, entity=entity, clean='delete')
@@ -58,13 +83,18 @@ def cross_show(entity):
 
 
 def gm_create():
-    print client.gms_create(group_id=1)
+    print client.gms_create(group_id=1, body={'objfile': {'subtype': 'appfile', 'version': '20180104.001'}})
 
 def gm_delete(entity):
     print client.gm_delete(group_id=1, entity=entity, clean='delete')
 
 def gm_show(entity):
     print client.gm_show(group_id=1, entity=entity, detail=True)
+
+
+# file_create()
+# file_index()
+# send_file(uuid='a1ed7026-c7a4-4bde-b031-4b4106386c2e')
 
 # group_create_test()
 # group_index_test()
@@ -79,7 +109,9 @@ def gm_show(entity):
 # cross_show(entity=1)
 # cross_delete(1)
 
-
-gm_create()
+# gm_create()
 # gm_delete(entity=2)
 # gm_show(entity=2)
+
+
+print client.quotes(endpoint='gogamechen1', entitys=[1,2,3,4,5])
